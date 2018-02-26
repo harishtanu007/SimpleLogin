a9a551f92901fc56393699f20d63e7a913a64b9c
HarishTanu <harishtanu007@github.com>
2018-02-26 23:18:38 +0530

    Initial commit

---
+++ SimpleLogin/MyLoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SimpleLogin
+{
+    public partial class MyLoginPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if(username.Text=="admin" && password.Text=="12345")
+            {
+                DisplayAlert("success!","correct credentials","ok");
+            }
+            else{
+                DisplayAlert("Failure","wrong credentials","Try Again");
+            }
+        }
+
+        public MyLoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleLogin/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SimpleLogin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
